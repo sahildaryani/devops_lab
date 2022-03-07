a403b441459ba4189cc672b5b2ba9c360394b10a
Sahil Daryani <56110441+sahildaryani@users.noreply.github.com>
2022-03-07 11:57:51 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld{
+	public static void main(String[] args)
+	{ 
+		for (int i=0;i<10;i++) 
+		{
+			System.out.println(i);
+			System.out.println("Helloworld"); 
+		} 
+	}
+}
